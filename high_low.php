43df1ff9d77cbf24960863172d33e5e388de9b02
Johnathan Christopherson <johnathan.christopherson@live.com>
2015-07-14 10:20:00 -0500

    Arguments

--- high_low.php
+++ high_low.php
@@ -1,17 +1,39 @@
 <?php 
 	
-	$rand = rand(0, 100);
-	echo $rand. PHP_EOL;
 	$guessed = 1;
+	$confirm = 'yes';
+	$userInput2 = 'yes';
 
+	if ($argc >= 3) {
+		$rand = rand($argv[1], $argv[2]);
+	}
+	else {
+		$rand = rand(0, 100);
+	}
 
-
+	echo $rand. PHP_EOL;
 	do {
 		fwrite(STDOUT, 'Guess? ');
 		$userInput = fgets(STDIN);
+		if ($guessed >= 7) {
+			echo 'You lose!';
+			break;
+		}
 		if ($userInput == $rand) {
 
 			fwrite(STDOUT, "You Win!\n");
+			echo "You guessed $guessed times!" . PHP_EOL;
+			fwrite(STDOUT, 'Type yes to play again! ');
+		    $userInput2 = trim(fgets(STDIN));
+		    echo $userInput2;
+		    if ($argc >= 3) {
+				$rand = rand($argv[1], $argv[2]);
+			}
+			else {
+				$rand = rand(0, 100);
+			}
+		    $rand;
+		    echo $rand. PHP_EOL;
 		}
 		else if ($userInput <= $rand) {
 			echo "Higher" . PHP_EOL;
@@ -21,24 +43,5 @@
 			echo "Lower" . PHP_EOL;
 			$guessed++;
 		} 
-	} while ($userInput != $rand); {
-		echo "You guessed $guessed times!" . PHP_EOL;
-	}
-
-
-
-		
-
-		
-
-
-
-
-
-
-
-
-
-
-
+	} while ($userInput2 == $confirm);  
  ?>
